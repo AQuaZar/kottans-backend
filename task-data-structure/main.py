d83edf1c9b9dc887ba68094e13bf2f93f93a2d94
AQuaZar <greenskullcompany@gmail.com>
2019-12-01 00:04:34 +0200

    Add example in main.py

--- task-data-structure/main.py
+++ task-data-structure/main.py
@@ -3,7 +3,15 @@ import json
 
 url = "http://localhost:8000/"
 
-
+data = {"data_type": "linked_list", "action": "insert", "value": "Jotaro"}
+data = json.dumps(data)
+r = requests.put(url, data)
+data = {"data_type": "linked_list", "action": "insert", "value": "Joseph"}
+data = json.dumps(data)
+r = requests.put(url, data)
+data = {"data_type": "linked_list", "action": "insert", "value": "Jonathan"}
+data = json.dumps(data)
+r = requests.put(url, data)
 data = {
     "data_type": "linked_list",
     "action": "insert",
