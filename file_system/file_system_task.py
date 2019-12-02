1f41295badd59ae3062b233e948623e00b4c2623
AQuaZar <greenskullcompany@gmail.com>
2019-12-03 01:14:35 +0200

    Complete counter task

--- file_system/file_system_task.py
+++ file_system/file_system_task.py
@@ -1,9 +1,20 @@
+import os
+import sys
 from pathlib import Path
 
-directory = Path("./")
-if "counter.txt" in directory.iterdir():
-    print(True)
+fileName = Path("counter.txt")
+if fileName.is_file():
+    with open("counter.txt", "r+") as f:
+        content = f.read()
+        if content.isnumeric():
+            f.seek(0)
+            f.write(str(int(content) + 1))
+        else:
+            sys.exit(1)
 else:
     with open("counter.txt", "w") as f:
         data = "1"
         f.write(data)
+        os.chmod("counter.txt", 0o777)
+        sys.exit(0)
+
